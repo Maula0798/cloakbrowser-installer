a625ea35f8bdf15330e4d9d09560068fb111c269
keonggaspoll <95425941+Maula0798@users.noreply.github.com>
2026-08-08 08:23:19 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+echo "=========================================="
+echo " CLOAKBROWSER INSTALLER"
+echo "=========================================="
+echo
+echo "GitHub installer berhasil dijalankan!"
+echo
